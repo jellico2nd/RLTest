6a1dfcc5dbc613cf992705766140182e0cac568d
Bartosz Laga <bartoszlaga@gmail.com>
2019-04-24 21:46:08 +0100

    Solution Created

---
+++ MaturingPolicies/MaturingPolicies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaturingPolicies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
